c57b20a8d785f9a4508dfebaefe2863f1abdc98a
tat16555 <pannatat.b@gmail.com>
2024-02-15 14:09:49 +0700

    first commit

--- Lab8.py
+++ Lab8.py
@@ -6,18 +6,23 @@ class Mall:
     def calculate_discount(self):
         if self.product_price < 3000:
             self.dis = self.product_price * 20 / 100
+            self.dis_per = 20
         elif self.product_price < 8000:
             self.dis = self.product_price * 30 / 100
+            self.dis_per = 30
         elif self.product_price < 15000:
             self.dis = self.product_price * 40 / 100
+            self.dis_per = 40
         else:
             self.dis = self.product_price * 10 / 100
+            self.dis_per = 10
 
         self.price_dis = self.product_price - self.dis
 
     def display_info(self):
         print("--------------------------------------------------")
         print(f"Product prices before discount: {int(self.product_price)}")
+        print(f"discount percentage : {self.dis_per}% ")
         print(f"Product discount: {self.dis}")
         print(f"Product price after discount: {self.price_dis:.1f}")
         print("--------------------------------------------------")
